25c9d2be057fca469fa6e6a5ff332c36a67c6595
Adan Partida <ada2029617@maricopa.edu>
2019-08-28 10:08:34 -0700

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Partida,Adan on 8/28/19.
+//  Copyright © 2019 Partida,Adan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
